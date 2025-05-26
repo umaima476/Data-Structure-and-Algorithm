836383372b7727511ec961f635803adc8953e5a9
Umaima Nawaz <umaimanawaz82@gmail.com>
2025-05-26 19:59:32 +0500

    Optimized BubbleSort implemented

--- Algorithms/OptimizedBubbleSort.py
+++ Algorithms/OptimizedBubbleSort.py
@@ -1,11 +1,18 @@
 def bubbleSort(array):
     for i in range(len(array)):
+        swapped = False
         for j in range(0, len(array)-1-i):
             if array[j] > array[j+1]:
                 temp = array[j]
                 array[j] = array[j+1]
                 array[j+1] = temp
+                swapped = True
+        if not swapped:
+            print("Given array is already sorted")
+            break
+    return array
 
-data = [-2, 45, 0, 11, -9]
+data = [9, 10, -2, 45, 11]
+data2 = [3, 4, 5, 6, 7]
 sorted = bubbleSort(data)
-print(f"Sorted Data is: {sorted}")
+print(sorted)
